b77b0d76a58816e9c0c4200992cd671f46ac8f62
OsayomoreEdugie <eosayomore@yahoo.com>
2021-10-23 11:49:59 +0200

    add new sensor class

---
+++ Models/Sensor.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Osayomore Edugie on 10/21/2021.
+//
+
+#include "Sensor.h"
+
+Sensor::Sensor(int id) {
+
+}
---
+++ Models/Sensor.h
@@ -0,0 +1,24 @@
+//
+// Created by Osayomore Edugie on 10/21/2021.
+//
+
+#ifndef HOMEDORK_DEVICESYSTEM_SENSOR_H
+#define HOMEDORK_DEVICESYSTEM_SENSOR_H
+#include "Device.h"
+#include "Arduino.h"
+
+class Sensor : public Device{
+    /**
+   * An class that represent a Sensor in a smart home this class inherits from Abstract Device class.
+   *
+   * Attributes:
+   *      @param id an integer who's value is the id of an instance of a subclass device.
+   *
+   * Methods:
+   * **/
+public :
+    Sensor( int id);
+
+};
+
+#endif //HOMEDORK_DEVICESYSTEM_SENSOR_H
